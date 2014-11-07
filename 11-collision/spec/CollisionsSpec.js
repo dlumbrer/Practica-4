a35c989bbd84ef8e9963eb63c8a43df0fc8462da
David Moreno Lumbreras <dmorenolumb@gmail.com>
2014-11-07 14:31:49 +0100

    Añadida prueba de muerte de nave por colision con alien

--- 11-collision/spec/CollisionsSpec.js
+++ 11-collision/spec/CollisionsSpec.js
@@ -84,3 +84,65 @@
     jugador, desaparece también.
 
 */
+
+describe("Collision", function(){
+	var canvas, ctx;
+
+	beforeEach(function(){
+	loadFixtures('index.html');
+
+	canvas = $('#game')[0];
+	expect(canvas).toExist();
+
+	ctx = canvas.getContext('2d');
+	expect(ctx).toBeDefined();
+
+	oldGame = Game;
+	SpriteSheet = {
+		map : {missile: { sx: 0, sy: 30, w: 2, h: 10, frames: 1 },
+			  ship: { sx: 0, sy: 0, w: 37, h: 42, frames: 1 },
+			  enemy_purple: { sx: 37, sy: 0, w: 42, h: 43, frames: 1 },
+			  enemy_bee: { sx: 79, sy: 0, w: 37, h: 43, frames: 1 },
+			  enemy_ship: { sx: 116, sy: 0, w: 42, h: 43, frames: 1 },
+			  enemy_circle: { sx: 158, sy: 0, w: 32, h: 33, frames: 1 },
+			  fireball: { sx: 0, sy: 64, w: 64, h: 64, frames: 12},
+			  },
+	  draw: function(ctx, name, x, y){},
+	};
+
+	});
+
+	afterEach(function(){
+	Game = oldGame;
+	}); 
+/*
+  it("La bola destruye pero no desparece",function(){
+	var board = new GameBoard();
+	alien = new Enemy({ x: -31, y: -64, sprite: 'enemy_purple', B: 100, C: 2, E: 100, health: 20  });
+	var bolafuego = new FireBall(-4,70, false);
+
+
+	board.add(bolafuego);
+	board.add(alien);
+
+	board.step(1);
+
+	expect(bolafuego.y).toBe(1);
+	expect(bolafuego.x).toBe(1);
+	expect(board.objects.length).toBe(1);
+	expect(board.objects[0].sprite).toBe(bolafuego.sprite);
+  });
+*/
+  it("Muerte de jugador (colision nave<->alien)",function(){
+	var board = new GameBoard();
+	alien = new Enemy({ x: 1, y: 1, sprite: 'enemy_purple', B: 100, C: 2, E: 100, health: 20  });
+	var nave = new PlayerShip();
+	nave.x = 1;
+	nave.y = 1;
+	board.add(nave);
+	board.add(alien);
+	board.step(0.1);
+	expect(board.objects.length).toBe(0);
+  });
+  
+});
