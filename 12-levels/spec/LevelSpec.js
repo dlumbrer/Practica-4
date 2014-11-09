942ed80d10406c9e854ccf2634ed28d4c4005a36
David Moreno Lumbreras <dmorenolumb@gmail.com>
2014-11-09 20:03:41 +0100

    Añadido LevelSpec FIN

--- 12-levels/spec/LevelSpec.js
+++ 12-levels/spec/LevelSpec.js
@@ -51,3 +51,28 @@
         el nivel ni enemigos en el tablero de juegos.
 
 */
+
+
+describe("Levels", function(){
+	var canvas, ctx;
+	beforeEach(function () {
+		loadFixtures('index.html');
+		canvas = $('#game')[0];
+		expect(canvas).toExist();
+		ctx = canvas.getContext('2d');
+		expect(ctx).toBeDefined();
+		oldGame = Game;
+		SpriteSheet = {
+			map: {
+			ship: { sx: 0, sy: 0, w: 37, h: 42, frames: 1 },
+			missile: { sx: 0, sy: 30, w: 2, h: 10, frames: 1 },
+			enemy_purple: { sx: 37, sy: 0, w: 42, h: 43, frames: 1 },
+			fireball: { sx: 0, sy: 64, w: 64, h: 64, frames: 12},
+			explosion: { sx: 0, sy: 64, w: 64, h: 64, frames: 12 }
+			},
+		};
+	});
+
+
+  
+});
